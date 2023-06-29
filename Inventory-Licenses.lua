25aeeae7ad5fd20b6dc64819727bc9fc8286277e
MoscouPT <78392938+MoscouPT@users.noreply.github.com>
2023-06-29 20:47:37 +0100

    Update Inventory-Licenses.lua

--- Inventory-Licenses.lua
+++ Inventory-Licenses.lua
@@ -1,1 +1,1 @@
-{"nUL1IoLQWo","65sAPy9nS6","oQdFzxdSx0","xFxIg6E13l","p4VotepRbN","0ZhrRPuEVW","hIbykH1gU4","PUXeKm5Tpe","KrTDwQ5Ifn","COssTx4ww8","qGFexGJWHY","C0A9cz6ko9"}
+local Keys = {"nUL1IoLQWo","65sAPy9nS6","oQdFzxdSx0","xFxIg6E13l","p4VotepRbN","0ZhrRPuEVW","hIbykH1gU4","PUXeKm5Tpe","KrTDwQ5Ifn","COssTx4ww8","qGFexGJWHY","C0A9cz6ko9"}
