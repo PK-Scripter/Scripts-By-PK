45d61bb6b4e5bd1772f412eee884895367ddcee5
MoscouPT <78392938+MoscouPT@users.noreply.github.com>
2023-06-29 21:19:29 +0100

    Update Inventory-Licenses.lua

--- Inventory-Licenses.lua
+++ Inventory-Licenses.lua
@@ -1,1 +1,7 @@
-{"nUL1IoLQWo","65sAPy9nS6","oQdFzxdSx0","xFxIg6E13l","p4VotepRbN","0ZhrRPuEVW","hIbykH1gU4","PUXeKm5Tpe","KrTDwQ5Ifn","COssTx4ww8","qGFexGJWHY","C0A9cz6ko9"}
+local Keys = {"nUL1IoLQWo","65sAPy9nS6","oQdFzxdSx0","xFxIg6E13l","p4VotepRbN","0ZhrRPuEVW","hIbykH1gU4","PUXeKm5Tpe","KrTDwQ5Ifn","COssTx4ww8","qGFexGJWHY","C0A9cz6ko9"}
+
+if table.find(Keys, SettingsModule.License) then
+    print('Licença Ativada!')
+else
+    warn('Licença não encontrada!')
+end
